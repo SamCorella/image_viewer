4baf72f19148eb2d475bad20dd1465dfd71acbe3
Sam Corella <samcorella77@gmail.com>
2023-03-10 15:24:26 -0500

    Begin implementing Select Image button

--- image_viewer.py
+++ image_viewer.py
@@ -1,15 +1,28 @@
 import tkinter as tk
+from tkinter import filedialog as fd
+from PIL import Image, ImageTk
 
 window = tk.Tk()
 window.title("Image Viewer")
 window.resizable(width=False, height=False)
 
-lbl_image = tk.Label()
+def select_image():
+    filename = fd.askopenfilename(
+        mode="r",
+        initialdir="/",
+        )
+    
+    load = Image.open(filename)
+    img = ImageTk.PhotoImage(load)
+    lbl_image = tk.Label(image=img)
+    lbl_image.grid(row=0, column=0)
+
 frm_buttons = tk.Frame(master=window)
 
 btn_enter = tk.Button(
     master=frm_buttons,
     text="Select Image",
+    command=select_image,
     )
 
 btn_back = tk.Button(
@@ -23,7 +36,6 @@ btn_forward = tk.Button(
     )
 
 frm_buttons.grid(row=1, column=0)
-
 btn_back.grid(row=0, column=0)
 btn_enter.grid(row=0, column=1)
 btn_forward.grid(row=0, column=2)
