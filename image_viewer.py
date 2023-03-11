48a024350d3f18056acc870b005726befa836fa5
Sam Corella <samcorella77@gmail.com>
2023-03-10 22:59:51 -0500

    Implement Back and Forward buttons

--- image_viewer.py
+++ image_viewer.py
@@ -6,38 +6,63 @@ window = tk.Tk()
 window.title("Image Viewer")
 window.resizable(width=False, height=False)
 
-def select_image():
+images = []
+cursor = 0
+
+def open_image():
+    global cursor
     filename = fd.askopenfilename(
         title="Select an image",
-        initialdir="/",
+        initialdir="/Users/samc/Pictures",
         )
-    
+
     img = ImageTk.PhotoImage(Image.open(filename))
-    lbl_image = tk.Label(master=window, image=img)
+    images.append(img)
+    cursor = len(images) - 1
+    display_image(img)
+
+def display_image(img):
+    lbl_image = tk.Label(image=img)
     lbl_image.grid(row=0, column=0)
-    lbl_image.image = img
 
+def next_image():
+    global cursor
+    if cursor < len(images) - 1:
+        cursor += 1
+        lbl_image.grid_forget()
+        display_image(images[cursor])
+
+def previous_image():
+    global cursor
+    if cursor > 0:
+        cursor -= 1
+        lbl_image.grid_forget()
+        display_image(images[cursor])
+
+lbl_image = tk.Label()
 frm_buttons = tk.Frame(master=window)
-
-btn_enter = tk.Button(
-    master=frm_buttons,
-    text="Select Image",
-    command=select_image,
-    )
 
 btn_back = tk.Button(
     master=frm_buttons,
     text="<<",
+    command=previous_image,
+    )
+
+btn_addImage = tk.Button(
+    master=frm_buttons,
+    text="Add Image",
+    command=open_image,
     )
 
 btn_forward = tk.Button(
     master=frm_buttons,
     text=">>",
+    command=next_image,
     )
 
 frm_buttons.grid(row=1, column=0)
 btn_back.grid(row=0, column=0)
-btn_enter.grid(row=0, column=1)
+btn_addImage.grid(row=0, column=1)
 btn_forward.grid(row=0, column=2)
 
 window.mainloop()
